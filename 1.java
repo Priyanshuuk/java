a9abf43d33d371f7c6f96e15204a3261f20e6432
Priyanshuuk <138270757+Priyanshuuk@users.noreply.github.com>
2025-02-12 16:24:40 +0530

    Add files via upload
    #first program
    To learning basics of java class and print out some basics details using comands
    
    #learning 
    Leaned about args and classes in java

---
+++ 1.java
@@ -0,0 +1,14 @@
+class ABC{
+     public static void main(String[] args ) {
+
+        String name = args[0];
+        int sem = Integer.parseInt(args[1]);
+        int uniroll = Integer.parseInt(args[2]);
+        String course = args[3];
+        System.out.println("semester is :" +sem);
+        System.out.println("name is :" +name);
+         System.out.println("roll no is : " +uniroll);
+          System.out.println("couse is " + course);
+
+     }
+}
