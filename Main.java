535a09d206ab6f1cec8fd29b6fcc49fe3a2cd9b2
Priyanshuuk <138270757+Priyanshuuk@users.noreply.github.com>
2025-02-20 17:33:40 +0530

    Update Main.java
    added a loop to visualise the pattern better

--- Main.java
+++ Main.java
@@ -6,17 +6,25 @@ public class Main {
      String one = sc.nextLine();
      int count = 0 ;
      int i = 0 ;
+     int s = -1;
+     int e = -1;
      while(i <one.length()-1){
        if(one.charAt(i) =='0'){
          i++;
+         s = i;
          while(one.charAt(i)=='1'){
            i++;
          }
+         e = i ;
          count+=1;
         
        }
         if(one.charAt(i)=='1'){i++;}
-         System.out.println(one);
+
+        for(int j = s-1 ; j < e+1; j++  ){
+         System.out.print(one.charAt(j));
+        }
+        System.out.println();
           System.out.println(count);
          
           
