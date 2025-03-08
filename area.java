3d5c346ca1188581dcb8eee9e85c48896d4745df
Priyanshuuk <138270757+Priyanshuuk@users.noreply.github.com>
2025-03-08 09:07:16 +0530

    Update area.java

--- area.java
+++ area.java
@@ -1,3 +1,4 @@
+import java.util.*;
 abstract class Shape {
     abstract int rectangle(int l, int b);
     abstract float circle(int r);
@@ -5,7 +6,7 @@ abstract class Shape {
 }
 
 class Area extends Shape {
-    
+
     int rectangle(int l, int b) {
         return l * b;
     }
@@ -15,14 +16,20 @@ class Area extends Shape {
         return (float) (Math.PI * r * r);  
     }
 
-  
+
     int square(int s) {
         return s * s;
     }
 
     public static void main(String args[]) {
-        Area a = new Area();
-        System.out.println("Area of circle with radius 5: " + a.circle(5));
+          Scanner sc = new Scanner(System.in);
+          int l = sc.nextInt();
+          int b = sc.nextInt();
+          int sql = sc.nextInt();
+          int r = sc.nextInt();
+        Shape shape = new Area();     
+        System.out.println("Area of circle with radius 5: " + shape.circle(r));  
+        System.out.println("Area of square with radius 5: " + shape.square(sql));  
+        System.out.println("Area of rectangle " + shape.rectangle(l,b));  
     }
 }
-
